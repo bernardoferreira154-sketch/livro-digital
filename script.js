afb3f71d7f2439e39b64e7b86c17273ac3081d25
bernardoferreira154-sketch <bernardo_ferreira154@estudante.sesisenai.org.br>
2026-09-01 19:53:25 -0300

    terminando atividade

---
+++ script.js
@@ -0,0 +1,64 @@
+// Passo 8: variáveis, arrays e objetos
+const contos = [
+    {
+      id: 0,
+      titulo: "O Conto dos Três Irmãos",
+      resumo: "Três irmãos usam magia para ultrapassar a Morte, que lhes concede três objetos poderosos."
+    },
+    {
+      id: 1,
+      titulo: "O Coração Peludo do Mago",
+      resumo: "Um jovem mago decide fechar seu coração para o amor usando magia das trevas."
+    },
+    {
+      id: 2,
+      titulo: "Babbitty, a Coelha e seu Toco Gargalhador",
+      resumo: "Um rei charlatão tenta aprender magia e é enganado por uma lavadeira mística."
+    }
+  ];
+  
+  // Passo 10: troca de Skins atualizando o body
+  function mudarCasa(nomeDaSkin) {
+    document.body.className = nomeDaSkin;
+  }
+  
+  // Passo 9: alterando o display dos containers
+  function abrirLivro() {
+    document.getElementById("tela-capa").style.display = "none";
+    document.getElementById("tela-livro").style.display = "block";
+  }
+  
+  // Passo 11: document.createElement e appendChild
+  function carregarMenu() {
+    const menuContainer = document.getElementById("lista-botoes");
+  
+    contos.forEach(conto => {
+      const botao = document.createElement("button");
+      botao.innerText = conto.titulo;
+
+
+      botao.onclick = () => selecionarConto(conto.id, botao);
+      menuContainer.appendChild(botao);
+    });
+  }
+  
+  function selecionarConto(id, botaoSelecionado) {
+    // Passo 12: array find()
+    const conto = contos.find(c => c.id === id);
+  
+    // Passo 9: innerText e style.display
+    document.getElementById("contoTitulo").innerText = conto.titulo;
+    document.getElementById("contoResumo").innerText = conto.resumo;
+    document.getElementById("contoNota").style.display = "block";
+  
+    // Passo 12 e 14: forEach para remover a classe active e adicionar ao botão selecionado
+    const botoes = document.querySelectorAll("#lista-botoes button");
+    botoes.forEach(b => b.classList.remove("active"));
+  
+    botaoSelecionado.classList.add("active");
+  }
+  
+  // Passo 13: window.onload
+  window.onload = function() {
+    carregarMenu();
+  };
